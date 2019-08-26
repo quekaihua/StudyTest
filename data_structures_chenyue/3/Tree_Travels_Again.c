145f2cb07b5285aa931979a57b47eb031ab87731
qkh <myshengun@163.com>
2019-08-26 08:43:47 +0800

    ..

--- data_structures_chenyue/3/Tree_Travels_Again.c
+++ data_structures_chenyue/3/Tree_Travels_Again.c
@@ -24,7 +24,8 @@ void push(Stack s, ElementType data);
 ElementType pop(Stack s);
 
 
-void PostOrderByOrders(int *InOrder, int *PreOrder, int length);
+void PostOrderByOrders(int preL, int inL, int PostRoot, int length);
+
 int main()
 {
 	int length;
@@ -45,9 +46,10 @@ int main()
 			InOrder[j++] = pop(S);
 		}
 	}
-	PostOrderByOrders(InOrder, PreOrder, length);
-	for(int i=0; i<cnt; i++){
-		if (i==cnt-1) {
+	PostOrderByOrders(0, 0, 0, length);
+
+	for(int i=0; i<length; i++){
+		if (i==length-1) {
 			printf("%d", result[i]);
 		} else {
 			printf("%d ", result[i]);
@@ -56,21 +58,21 @@ int main()
 	return 0;
 }
 
-void PostOrderByOrders(int * InOrder, int * PreOrder, int length)
+void PostOrderByOrders(int preL, int inL, int PostL, int length)
 {
-	if(length<1) return;
-	int i;
-	int * orgIn;
-	orgIn = InOrder;
-	for(i=0; i<length; i++){
-		if (*InOrder == *PreOrder) break;
-		InOrder++;
-	}
+	if(length == 0) return;
+	if(length == 1) {result[PostL] = PreOrder[preL]; return;}
+	int root = PreOrder[preL];
+	result[PostL+length-1] = root;
+	int i, L, R;
+	for(i=0; i<length; i++)
+		if (InOrder[inL+i] == root) break;
+	L = i;
+	R = length-i-1;
 	//left
-	PostOrderByOrders(orgIn, PreOrder+1, i);
+	PostOrderByOrders(preL+1, inL, PostL, L);
 	//right
-	PostOrderByOrders(orgIn+i+1, PreOrder+1+i, length-i-1);
-	result[cnt++] = *InOrder;
+	PostOrderByOrders(preL+L+1, inL+L+1, PostL+L, R);
 }
 
 Stack InitialStack()
@@ -80,6 +82,7 @@ Stack InitialStack()
 		exit(1);
 	s->size = 0;
 	s->top = NULL;
+	return s;
 }
 
 void push(Stack s, ElementType data)
