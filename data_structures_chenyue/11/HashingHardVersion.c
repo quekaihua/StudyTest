e3310b8962211f65584b4d40568ba99a9ce41d13
kh <myshengun163.com>
2019-08-28 18:50:26 +0800

    ..

--- data_structures_chenyue/11/HashingHardVersion.c
+++ data_structures_chenyue/11/HashingHardVersion.c
@@ -1,113 +1,188 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #define MAXSIZE 1000
 // int Table[MAXSIZE];
+int Queue[MAXSIZE];
 int Table[11] = {33, 1, 13, 12, 34, 38, 27, 22, 32, -1, 21};
-int Table_2[MAXSIZE];
-int Visited[MAXSIZE];
-int result[MAXSIZE];
+int Indegree[MAXSIZE] = {0};
+int Graph[MAXSIZE][MAXSIZE];
 
+typedef int ElementType;
+typedef struct HeapStruct *MinHeap;
+struct HeapStruct{
+	ElementType *Elements;
+	int Size;
+	int Capacity;
+};
 typedef int Position, ElementType;
 int N;
 
 Position Find(ElementType Key, int TableSize);
-void InsertTable(int TableSize);
-void InitialTable(int TableSize);
-void InitialVisited(int TableSize);
-int CheckFull(int TableSize);
-Position Find_Index(ElementType Key, int TableSize);
+void TopSort(int N);
+void InitialGraph(int N);
+void BuildGraph();
+int compare ( const void *a , const void *b);
+ElementType DeleteMin(MinHeap H);
+void BuildMinHeap(MinHeap H);
+void Insert(MinHeap H, ElementType node);
+int IsEmpty(MinHeap H);
+int HeapIsFull(MinHeap H);
+MinHeap CreateHeap();
 
 int main()
 {
 	// scanf("%d", &N);
 	N = 11;
-	InitialVisited(N);
-	InitialTable(N);
 	// for (int i=0; i<N; i++) {
 	// 	scanf("%d", Table[i]);
 	// }
-	InsertTable(N);
+
+	InitialGraph(N);
+	BuildGraph(N);
+	for(int i=0; i<N; i++) {
+		printf("Indegree[%d]=%d ", i, Indegree[i]);
+	}
+	printf("\n");
+	TopSort(N);
+
 	return 0;
 }
 
-void InitialVisited(int N)
+void BuildGraph(int N)
 {
-	for (int i=0; i<N; i++) {
-		Visited[i] = 0;
+	for(int i=0; i<N; i++) {
+		Position p = Table[i] % N;
+		if ( Table[p] != Table[i] && Table[i] != -1) { //如果这个位置已被占有
+			for (int j = p; j != i; j = ( j + 1 ) % N ) {
+				Graph[j][i] = 1;
+				Indegree[i]++; 	//位置j到i的元素都在i之前进入哈希表
+			}
+		}
 	}
 }
 
-void InitialTable(int N)
+void TopSort(int N)
 {
-	for (int i=0; i<N; i++) {
-		Table_2[i] = 0;
-	}
-}
-
-void InsertTable(int TableSize)
-{	
-	int Key, cnt;
-	while(!CheckFull(TableSize)){
-		cnt = 0;
-		for (int i=0; i<N; i++) {
-			Key = Table[i];
-			if(Key == -1){
-				Visited[i] = 1;
-				continue;
-			}
-			Position pos1;
-			Position pos = Find(Key, TableSize);
-			pos1 =  Find_Index(Key, TableSize);
-			if (pos == pos1) {
-				result[cnt++] = Key;
-				Visited[pos]  = 1;
-			} else {
-				continue;
-			}
+	int front, rear;
+	int V;
+	front = rear = 0;
+	MinHeap H = CreateHeap();
+	for (int i=0; i<N; i++)
+		if(Table[i] != -1 && Indegree[i] == 0) {  //度为0的入队
+			Queue[rear++] = i;
+			Insert(H, Table[i]);
 		}
-		for (int i=0; i<cnt; i++) {
-			Key = result[i];
-			Position pos = Find(Key, TableSize);
-			Table_2[pos] = Key;
-			printf("%d", result[i]);
-			if (i!=cnt-1) printf(" ");
-		}
+		BuildMinHeap(H);
+		for(int i=0; i<H->Size; i++) 
+		printf("Ele[%d]=%d ", i, H->Elements[i]);
 		printf("\n");
-	}
-}
-
-int CheckFull(int TableSize)
-{
-	for (int i=0; i<TableSize; i++) {
-		if (!Visited[i]) return 0;
-	}
-	return 1;
-}
-
-Position Find(ElementType Key, int TableSize)
-{
-	Position pos = Key % TableSize;
-	int cnum = 1;  //冲突次数
-	while ( cnum < TableSize ) {  //k>=size后，平方探测将进入一个死循环
-		if ( Table_2[pos] == 0 ) { //如果该位置没有元素
-			return pos;
-		}
-		else { //该位置有元素，使用平方探测法解决冲突
-			pos = ( pos + 1) % TableSize;
-			cnum++;
+	int data;
+	while(front<rear) {
+		V = Queue[front++];
+		data = DeleteMin(H);
+		// printf("data=%d ", data);
+		for (int j=0; j<N; j++) {
+			if(Graph[V][j] != 0) {
+				if (--Indegree[j] == 0) {  //度为0的入队
+					Queue[rear++] = j;
+					Insert(H, Table[j]);
+					BuildMinHeap(H);
+					for(int i=0; i<H->Size; i++) 
+		printf("Ele[%d]=%d ", i, H->Elements[i]);
+		printf("\n");
+				}
+			}
 		}
 	}
-	return -1;
+	while(!IsEmpty(H)){
+		V = DeleteMin(H);
+		printf("V=%d ", V);
+	}
 }
 
-Position Find_Index(ElementType Key, int TableSize)
+void InitialGraph(int N)
 {
-	for (int i=0; i<TableSize; i++) {
-		if(Table[i] == -1) continue;
-		if(Table[i] == Key) return i;
+	for (int i=0; i<N; i++)
+		for (int j=0; j<N; j++){
+			Graph[i][j] = 0;
+		}
+}
+
+int compare ( const void *a , const void *b)
+{
+	return *(int *)b - *(int *)a;
+}
+
+MinHeap CreateHeap()
+{
+	MinHeap H = (MinHeap)malloc(sizeof(struct HeapStruct));
+	H->Size   = 0;
+	H->Capacity = MAXSIZE;
+	H->Elements = malloc(MAXSIZE * sizeof(int));
+	return H;
+}
+
+int HeapIsFull(MinHeap H)
+{
+	return H->Size+1 == H->Capacity ? 1 : 0;
+}
+
+int IsEmpty(MinHeap H)
+{
+	return H->Size == 0 ? 1 : 0;
+}
+
+void Insert(MinHeap H, ElementType node)
+{
+	if (HeapIsFull(H)) return;
+	H->Elements[H->Size++] = node;
+}
+
+void BuildMinHeap(MinHeap H)
+{
+	int parent, Child, LeftChild;
+	ElementType temp;
+	for (int i=H->Size/2-1; i>=0; i-- ) {
+		for (int Child=i; 2*Child+1<H->Size; Child = Child) {
+			LeftChild = 2 * i + 1;
+			if(LeftChild < H->Size-1 && 
+			H->Elements[LeftChild] > H->Elements[LeftChild+1])
+				Child = ++LeftChild;
+			else 
+				Child = LeftChild;
+			if(H->Elements[Child] < H->Elements[i]) {
+				temp = H->Elements[i];
+				H->Elements[i] = H->Elements[Child];
+				H->Elements[Child] = temp;
+			} else break;
+		}
 	}
-	return -1;
+}
+
+ElementType DeleteMin(MinHeap H)
+{
+	int Parent, Child;
+	ElementType MinItem, temp;
+	if(IsEmpty(H)) {
+		printf("MinHeap is empty.\n");
+		return -1;
+	}
+	MinItem = H->Elements[0];
+	temp = H->Elements[H->Size-1];
+	for (Parent=0; Parent*2+1 < H->Size; Parent=Child) {
+		Child = Parent * 2 + 1;
+		if ((Child < H->Size-1) &&
+			H->Elements[Child] > H->Elements[Child+1])
+			Child++;
+		if(temp < H->Elements[Child]) break;
+		else
+			H->Elements[Parent] = H->Elements[Child];
+	}
+	H->Elements[Parent] = temp;
+	H->Size--;
+	return MinItem;
 }
 
 
--- data_structures_chenyue/5/HuffmanCodes.c
+++ data_structures_chenyue/5/HuffmanCodes.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
 
-typedef struct TreeNode* ElementType;
+#define MAXSIZE 65
+// ElementType Codes[MAXSIZE];
+int f[MAXSIZE];
+
+typedef struct TreeNode *ElementType;
 typedef struct TreeNode *HuffmanTree;
 struct TreeNode {
-	int Weight;
+	char code;
+	int  Weight;
 	HuffmanTree Left, Right;
 };
 
@@ -14,26 +22,149 @@ struct HeapStruct{
 	int Capacity;
 };
 
+ElementType CreateNode();
+MinHeap CreateHeap();
+int HeapIsFull(MinHeap H);
+int IsEmpty(MinHeap H);
+void Insert(MinHeap H, ElementType node);
+ElementType DeleteMin(MinHeap H);
+HuffmanTree Huffman(MinHeap H, int N);
+int TreeWeight(HuffmanTree T, int depth);
+int GetCodeLength(char code[][MAXSIZE], int N);
+int BuildHuffman(char code[][MAXSIZE], int N);
+
 int main()
 {
 	int N, M;
-	scanf("%d", &N);
-	
+	scanf("%d ", &N);
+	ElementType node;
+	MinHeap H = CreateHeap();
+
+	//输入code
+	for(int i=0; i<N; i++) {
+		node = CreateNode();
+		scanf("%c %d ", &node->code, &node->Weight);
+		f[i] = node->Weight;
+		Insert(H, node);
+	}
+
+	// for(int i=0; i<H->Size; i++) {
+	// 	printf("%c %d\n", H->Elements[i]->code, H->Elements[i]->Weight);
+	// }
+
+	HuffmanTree T = Huffman(H, N);
+	int TotalWeight = TreeWeight(T, 0);
+
+	scanf("%d ", &M);
+	char c, code[MAXSIZE][MAXSIZE];
+	int flag, length;
+	//输入学生提交
+	for(int i=0; i<M; i++){
+		for(int j=0; j<N; j++) {
+			scanf("%c %s ", &c, code[j]);
+		}
+		// for(int j=0; j<N; j++) {
+		// 	printf("%c %s\n", c, code[j]);
+		// }
+		length = GetCodeLength(code, N);
+		if(length != TotalWeight) {
+			printf("No\n");
+		} else {
+			flag = BuildHuffman(code, N);
+			flag == 1 ? printf("Yes\n") : printf("No\n");
+		}
+	}
 }
 
-HuffmanTree Huffman(MinHeap H)
+int GetCodeLength(char code[][MAXSIZE], int N)
 {
-	int i; HuffmanTree T;
-	BuildMinHeap(H);
-	for(i=1; i< H->Size; i++) {
-		T = malloc(sizeof(struct TreeNode));
-		T->Left = DeleteMin(H);
-		T->Right = DeleteMin(H);
-		T->Weight = T->Left->Weight + T->Right->Weight;
-		Insert(H, T);
+	int length = 0;
+	for(int i=0; i<N; i++) {
+		length += strlen(code[i]) * f[i];
 	}
-	T = DeleteMin(H);
-	return T;
+	return length;
+}
+
+int BuildHuffman(char code[][MAXSIZE], int N)
+{
+	HuffmanTree T, Current, temp;
+	T = CreateNode();
+	for(int i=0; i<N; i++) {
+		Current = T;
+		for(int j=0; code[i][j] != '\0'; j++) {
+			if (code[i][j] == '0') {
+				if(Current->Left == NULL) Current->Left = CreateNode();
+				Current = Current->Left;
+			} else if(code[i][j] == '1'){
+				if(Current->Right == NULL) Current->Right = CreateNode();
+				Current = Current->Right;
+			}
+		}
+		if(Current->Left != NULL || Current->Right != NULL) return 0;
+	}
+	return 1;
+}
+
+ElementType CreateNode()
+{
+	ElementType node = (ElementType)malloc(sizeof(struct TreeNode));
+	node->Left = node->Right = NULL;
+	return node;
+}
+
+MinHeap CreateHeap()
+{
+	MinHeap H = (MinHeap)malloc(sizeof(struct HeapStruct));
+	H->Size   = 0;
+	H->Capacity = MAXSIZE;
+	H->Elements = malloc(MAXSIZE * sizeof(struct TreeNode));
+	return H;
+}
+
+int HeapIsFull(MinHeap H)
+{
+	return H->Size+1 == H->Capacity ? 1 : 0;
+}
+
+int IsEmpty(MinHeap H)
+{
+	return H->Size == 0 ? 1 : 0;
+}
+
+void Insert(MinHeap H, ElementType node)
+{
+	if (HeapIsFull(H)) return;
+	H->Elements[H->Size++] = node;
+}
+
+void BuildMinHeap(MinHeap H)
+{
+	int parent, Child, LeftChild;
+	ElementType temp;
+	for (int i=H->Size/2-1; i>=0; i-- ) {
+		// for(int i=0; i<H->Size; i++) {
+		// 	printf("%c %d ", H->Elements[i]->code, H->Elements[i]->Weight);
+		// }
+		// printf("\n");
+		for (int Child=i; 2*Child+1<H->Size; Child = Child) {
+			LeftChild = 2 * i + 1;
+			if(LeftChild < H->Size-1 && 
+			H->Elements[LeftChild]->Weight > H->Elements[LeftChild+1]->Weight)
+				Child = ++LeftChild;
+			else 
+				Child = LeftChild;
+			if(H->Elements[Child]->Weight < H->Elements[i]->Weight) {
+				temp = H->Elements[i];
+				H->Elements[i] = H->Elements[Child];
+				H->Elements[Child] = temp;
+			} else break;
+		}
+		// for(int i=0; i<H->Size; i++) {
+		// 	printf("%c %d ", H->Elements[i]->code, H->Elements[i]->Weight);
+		// }
+		// printf("\n");
+		// printf("paixu yici\n");
+	}
 }
 
 ElementType DeleteMin(MinHeap H)
@@ -42,20 +173,48 @@ ElementType DeleteMin(MinHeap H)
 	ElementType MinItem, temp;
 	if(IsEmpty(H)) {
 		printf("MinHeap is empty.\n");
-		return;
+		return NULL;
 	}
-	MinItem = H->Elements[1];
-	temp = H->Elements[H->Size--];
-	for (Parent=1; Parent*2<=H->Size; Parent=Child) {
-		Child = Parent * 2;
-		if ((Child != H->Size) &&
+	MinItem = H->Elements[0];
+	temp = H->Elements[H->Size-1];
+	for (Parent=0; Parent*2+1 < H->Size; Parent=Child) {
+		Child = Parent * 2 + 1;
+		if ((Child < H->Size-1) &&
 			H->Elements[Child]->Weight > H->Elements[Child+1]->Weight)
 			Child++;
-		if(temp <= H->Elements[Child]) break;
+		if(temp->Weight < H->Elements[Child]->Weight) break;
 		else
 			H->Elements[Parent] = H->Elements[Child];
 	}
 	H->Elements[Parent] = temp;
+	H->Size--;
 	return MinItem;
 }
+
+HuffmanTree Huffman(MinHeap H, int N)
+{
+	BuildMinHeap(H);
+	int i; HuffmanTree T;
+	ElementType node;
+	for(i=1; i<N; i++) {
+		node = CreateNode();
+		node->Left = DeleteMin(H);
+		node->Right = DeleteMin(H);
+		node->Weight = node->Left->Weight + node->Right->Weight;
+		Insert(H, node);
+		BuildMinHeap(H);
+	}
+	T = DeleteMin(H);
+	return T;
+}
+
+int TreeWeight(HuffmanTree T, int depth)
+{
+	if	(T->Left == NULL && T->Right == NULL) {
+		// printf("weight=%d code=%c depth=%d weight*depth=%d\n", T->Weight, T->code, depth, T->Weight*depth);
+		return T->Weight * depth;
+	}
+	return (TreeWeight(T->Left, depth+1) + TreeWeight(T->Right, depth+1));
+}
+
 
