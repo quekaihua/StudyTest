d1edf249f6894850b8fadd714eb4899bf18ac4bf
kh <myshengun163.com>
2019-08-29 20:55:03 +0800

    ..

--- data_structures_chenyue/7/SavingJamesBond.c
+++ data_structures_chenyue/7/SavingJamesBond.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <math.h>
 
 typedef int Vertex;
 typedef int position;
@@ -9,251 +9,156 @@ typedef struct point *Point;
 struct point {
     position x;
     position y;
+	int d;
 };
 double D;
+int N;
 
 #define MAXSIZE 101
 
 int Graph[MAXSIZE][MAXSIZE];
 Point Points[MAXSIZE];
-int dist[MAXSIZE], path[MAXSIZE];
+int dist[MAXSIZE], path[MAXSIZE], Visited[MAXSIZE] = {0};
 int count = 0; //calculate points
 
-void InitialPath(int S);
-void InitialDist(int S);
-void InitialGraph();
-void BuildEdge();
-void InsertVertex(Point x);
-void InsertEdge(Point x, Point y, int i, int j);
-
-int Distance(Point X, Point Y);
-bool IsSave(Point X);
-void Unweighted (Vertex S);
-void PrintPath();
-void FirstMinimun(int V, int W);
+double Distance(int a, int b);
+void PrintPath( int Stack[], int top );
+int IsSafe( int v );
+int FirstJump( Point dot );
+int Jump( int a, int b );
+int ShortestPath(int S);
+void Save007();
 
 int main()
 {
-    InitialGraph();
-    int N;
     scanf("%d %lf", &N, &D);
     int x, y;
     for(int i=0; i<N; i++)
     {
         Point p = malloc(sizeof(struct point));
         scanf("%d %d", &x, &y);
-        p->x = x + 50;
-        p->y = y + 50;
-        InsertVertex(p);
+        p->x = x;
+        p->y = y;
+		p->d = x*x + y*y;
+       	Points[count++] = p;
     }
-    BuildEdge(D);
-
+    // BuildEdge(D);
     // Points = malloc(sizeof(Point)*MAXSIZE*MAXSIZE);
-    Unweighted(0);
-	PrintPath();
-
-    return 0;
+	//按离原点的距离从小到大排序
+	for(int i = 0; i < N - 1; i++){
+		int step = 0;
+		for(int j = 0; j < N - 1 - i; j++)
+			if(Points[j]->d > Points[j + 1]->d){
+				Point Tmp = Points[j];
+				Points[j] = Points[j + 1];
+				Points[j + 1] = Tmp;
+				step++;
+			}
+		if ( step == 0 ) break;
+	}
+	//007有超能力，能直接跳上岸
+	if( D >= 42.5 ) 
+		printf("1\n");	
+	else 
+		Save007();
+	return 0;
 }
 
-void InitialGraph()
+double Distance(int a, int b)
 {
-    for (int i=0; i<MAXSIZE; i++) {
-        for (int j=0; j<MAXSIZE; j++)
-            Graph[i][j] = 0;
-    }
-    Point a = malloc(sizeof(struct point));
-    a->x = a->y = 50;
-    Points[count++] = a;
+	Point X = Points[a];
+	Point Y = Points[b];
+    return sqrt((Y->x - X->x) * (Y->x - X->x) + (X->y - Y->y) * (X->y - Y->y));
 }
 
-void InitialPath(int S)
+int IsSafe( int v )
 {
-    for(int i=0; i<count; i++)
-        path[i] = i==S ? -1 : 0;
+	int x = abs( Points[v]->x );
+	int y = abs( Points[v]->y );
+	if( x + D >= 50 || y + D >= 50 )
+		return 1;
+	else return 0;
 }
 
-void InitialDist(int S)
+int FirstJump( Point dot )
 {
-	for(int i=0; i<count; i++)
-		dist[i] = i==S ? 0 : -1;
+	double temp = dot->x * dot->x + dot->y * dot->y;
+	double dis = sqrt( temp );
+	double cap = 7.5 + D;	//第一次跳要加上小岛的半径
+	if( cap >= dis && dis > 7.5) return 1;	//岸上的鳄鱼不算
+	else return 0;
 }
 
-void BuildEdge()
+int Jump( int a, int b )
 {
-    for(int i=0; i<count; i++){
-        for(int j=count-1; j>i;j--){
-            Point q = Points[i];
-            Point p = Points[j];
-            InsertEdge(p, q, i, j);
-        }
-    }
+	double dis = Distance(a, b);
+	if( D >= dis) return 1;
+	else return 0;
 }
 
-void InsertVertex(Point p)
+int ShortestPath(int S)
 {
-    Points[count++] = p;
-}
-
-void InsertEdge(Point X, Point Y, int i, int j)
-{
-    int distance = Distance(X, Y);
-    if((X->x==50 && X->y==50) || (Y->x==50 && Y->y==50)){
-        if(distance <= (D+7.5)*(D+7.5)){
-            Graph[i][j] = 1;
-            Graph[j][i] = 1;
-        }
-    } else if (distance <= D*D){
-        Graph[i][j] = 1;
-        Graph[j][i] = 1;
-    }
-}
-
-int Distance(Point X, Point Y)
-{
-    return (Y->x - X->x) * (Y->x - X->x) + (X->y - Y->y) * (X->y - Y->y);
-}
-
-bool IsSave(Point X){
-    Point Y = malloc(sizeof(struct point));
-    Y->x = X->x;
-    Y->y = 0;
-    if(Distance(X, Y) <= D*D){
-        free(Y);
-        return true;
-    }
-
-    Y->x = 0;
-    Y->y = X->y;
-    if(Distance(X, Y) <= D*D){
-        free(Y);
-        return true;
-    }
-
-    Y->x = 99;
-    Y->y = X->y;
-    if(Distance(X, Y) <= D*D){
-        free(Y);
-        return true;
-    }
-
-    Y->x = X->x;
-    Y->y = 99;
-    if(Distance(X, Y) <= D*D){
-        free(Y);
-        return true;
-    }
-
-    return false;
-}
-
-void Unweighted (Vertex S)
-{
-	InitialPath(S);
-	InitialDist(S);
-
+	for(int i = 0; i < count; i++)
+		dist[i] = path[i] = -1;
+	int V;
 	int rear, front, Queue[MAXSIZE];
 	rear = 0;
 	front = 0;
+	Visited[S] = 1;
+	dist[S] = 0;
+	if( IsSafe(S) ) return S; //007跳2次就能上岸（经过1条鳄鱼）
 	Queue[front++] = S;
-
-	Vertex V, i;
 	while(rear < front) {
 		V = Queue[rear++];
-		for(i=0; i<count; i++){
-			if(dist[i] != -1 && dist[V] + 1 == dist[i])
-				FirstMinimun(V, i);
-			if(Graph[V][i] && dist[i] == -1) {
+		for(int i = 0; i < N; i++){
+			if( !Visited[i] && Jump(V, i) ){
 				dist[i] = dist[V] + 1;
 				path[i] = V;
+				Visited[i] = 1;
+				if( IsSafe(i) ) return i;
 				Queue[front++] = i;
 			}
 		}
 	}
+	return -1;
 }
 
-void FirstMinimun(int V, int W)
+void Save007()
 {
-    int i, pre1, pre2;
-    
-    i = V; pre1 = V;
-    while(path[i] != 1){
-        pre1 = path[i];
-        i = pre1;
-    }
-    
-    i = W; pre2 = W;
-    while(path[i] != 1){
-        pre2 = path[i];
-        i = pre2;
-    }
-    
-    if(Distance(Points[pre1], Points[0]) < Distance(Points[pre2],Points[0]))
-        path[W] = V;
-}
+	int NumberOfJump = MAXSIZE, LastCro;
+	int Stack[MAXSIZE], top=0;
 
-void PrintPath()
-{
-	int j = 0, save[MAXSIZE];
-	for(int i=0; i<count; i++) {
-		Point ptr = Points[i];
-		if(IsSave(ptr)) {
-			save[j++] = i;
-		}
-	}
-	// for(int i=0; i<j; i++) {
-	// 	printf("save = %d, %d\n", Points[save[i]]->x, Points[save[i]]->y);
-	// }
-
-	int k = 0;
-	int min=-1, minPath[MAXSIZE];
-	for(int i=0; i<j; i++)
-	{
-		// printf("dist[%d]=%d ", i, dist[i]);
-		if((dist[save[i]] > -1 && dist[save[i]] < min) || 
-		i==0) {
-			min = dist[save[i]];
-		}
-	}
-	if(min==-1) {
-		printf("0");
-		return;
-	} else if (min==0) {
-		printf("1"); 
-		return;
-	}
-
-	for(int i=0; i<j; i++){
-		if(min == dist[save[i]]){
-			minPath[k++] = save[i];
-		}
-	}
-	int minPath_a, minPath_b, temp_a, temp_b, result;
-	for (int i=0; i<k; i++) {
-		for (int m=0; m<k; m++) {
-			// if(i==m) continue;
-			minPath_a = temp_a = minPath[i];
-			minPath_b = temp_b = minPath[m];
-			while(path[minPath_a] != 0) {
-				minPath_a = path[minPath_a];
-				minPath_b = path[minPath_b];
-			}
-			if(Distance(Points[minPath_a], Points[0]) < Distance(Points[minPath_b], Points[0])) {
-				result = temp_a;
-			} else {
-				result = temp_b;
+	for(int i = 0; i < N; i++) {
+		if ( !Visited[i] && FirstJump(Points[i]) ) {
+			LastCro = ShortestPath(i);
+			if( LastCro != -1 && NumberOfJump > dist[LastCro] ) { //如果有更短的路径
+				NumberOfJump = dist[LastCro];  //更新Jump次数
+				//将路径入栈保存（path每做一次单源最短路就要更新一次，因此要记录）
+				//错误原因：没记录路径，直接用了path来输出，导致无限循环，内存超限
+				while ( top != 0 ) //清空上一次的stack（stack没有clear方法）
+					top--;
+				
+				int v = LastCro;
+				while ( v != i ){
+					Stack[top++] = v;
+					v = path[v];
+				}
+				Stack[top++] = v;
 			}
 		}
 	}
-
-	int Stack[MAXSIZE], top=0, step=1;
-	while(path[result] != -1) {
-		Stack[top++] = result;
-		result = path[result];
-		step++;
+	if (NumberOfJump == MAXSIZE) printf("0\n"); //没有路径能到岸上
+	else {
+		printf("%d\n", NumberOfJump + 2); //加上在岛上跳的一次，和最后跳上岸的一次
+		PrintPath( Stack, top );
 	}
-	printf("%d\n", step);
-	for(int i=top-1; i>=0; i--) {
-		Point p = Points[Stack[i]];
-		printf("%d %d\n", p->x-50, p->y-50);
+}
+
+void PrintPath( int Stack[], int top )
+{
+	int temp;
+	while( top != 0 ) {
+		temp = Stack[--top];
+		printf("%d %d\n", Points[temp]->x, Points[temp]->y);
 	}
 }
--- data_structures_chenyue/8/ActivityOnEdge.c
+++ data_structures_chenyue/8/ActivityOnEdge.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define MAXSIZE 10000
-int Queue[MAXSIZE], Indegree[MAXSIZE], Outdegree[MAXSIZE], Earliest[MAXSIZE], Latest[MAXSIZE];
+#define MAXSIZE 105
+int Indegree[MAXSIZE], Outdegree[MAXSIZE], Earliest[MAXSIZE], Latest[MAXSIZE];
 int Graph[MAXSIZE][MAXSIZE];
+int idx;
 
-void TopSort(int N);
+int TopSort(int N);
 void InitialGraph(int N);
 void InitialIndegree(int N);
 void InsertEdge(int i, int j, int LastTime);
 void UpdateLatest(int N);
 int GetEndPoint(int N);
 void PrintKeyTask(int N);
+int getMax( int arr[], int N);
 
 int main()
 {
@@ -27,13 +29,6 @@ int main()
 		InsertEdge(point1, point2, LastTime);
 	}
 
-// for(int i=0; i<N; i++) {
-// 		printf("Indegree[%d]=%d ", i, Indegree[i]);
-// 	}
-	
-	TopSort(N);
-	UpdateLatest(N);
-
 	// printf("\n");
 	// for(int i=1; i<=N; i++) {
 	// 	printf("Indegree[%d]=%d ", i, Indegree[i]);
@@ -44,59 +39,51 @@ int main()
 	// 	printf("Outdegree[%d]=%d ", i, Outdegree[i]);
 	// }
 	// printf("\n");
-	// for(int i=1; i<=N; i++) {
-	// 	printf("Earliest[%d]=%d ", i, Earliest[i]);
-	// }
-	// printf("\n");
-	// for(int i=1; i<=N; i++) {
-	// 	printf("Latest[%d]=%d ", i, Latest[i]);
-	// }
-	// printf("\n");
 	//check Indegree
-	int flag = 0;
-	for(int i=1; i<=N; i++) {
-		if(Indegree[i] != 0) {
-			printf("0");
-			flag = 1;
-			break;
-		}
-	}
 
-	if(!flag) {
+	if( !TopSort(N) )
+		printf("0\n");
+	else {
 		int max;
 		for(int i=1; i<=N; i++) {
 			if(i==1 || max < Earliest[i])
 				max = Earliest[i];
 		}
 		printf("%d\n", max);
+		UpdateLatest(N);
+
 		PrintKeyTask(N);
 	}
+	
 	return 0;
 }
 
-void TopSort(int N)
+int TopSort(int N)
 {
-	int front, rear;
-	front=0;
-	rear=0;
-	int V;
+	int Queue[MAXSIZE] = {0}, front=0, rear=0;
+	int V, cnt=0;
 	for (int i=1; i<=N; i++)
-		if(Indegree[i] == 0) {
+		if(Indegree[i] == 0) {   //入度为0入队
 			Queue[rear++] = i;
 			Earliest[i]  = 0;
 		}
+
 	while(front < rear) {
 		V = Queue[front++];
+		cnt++;
 		// printf("V=%d ", V);
 		for (int j=1; j<=N; j++) {
 			if (Graph[V][j] != -1) {
-				if (Earliest[V] + Graph[V][j] > Earliest[j])
+				if (Earliest[V] + Graph[V][j] > Earliest[j])  //时间比较大则更新
 					Earliest[j] = Earliest[V] + Graph[V][j];
 				if (--Indegree[j] == 0)
 					Queue[rear++] = j;
 			}
 		}
 	}
+
+	if( cnt != N ) return 0;	//如果没有取出所有元素，说明图中有回路
+	else return 1;
 }
 
 void InitialGraph(int N)
@@ -128,42 +115,50 @@ void UpdateLatest(int N)
 {
 	int v, w;
 	int front, rear;
+	int Queue[MAXSIZE] = {0};
 	front = rear = 0;
-	while (1)
-	{
-		// printf("Graph[5][7]=%d, Graph[7][5]=%d\n", Graph[5][7], Graph[7][5]);
-		v = GetEndPoint(N);
-		if (v == -1) break;
-		Latest[v] = Earliest[v];
-		Queue[rear++] = v;
-		while(front < rear) {
-			w = Queue[front++];  //处理节点入队， 邻节点入队
-			// printf("w=%d\n");
-			for(int i=1; i<=N; i++) {
-				if(Graph[i][w] != -1) {//i为前节点 
-					// printf("i=%d\n", i);
+
+	for (int i=1; i<=N; i++) {
+		if(Outdegree[i] == 0) {   //入度为0入队
+			Queue[rear++] = i;
+		}
+	}
+
+	for( int i = 1; i <= N; i++ )
+		Latest[i] = INT_MAX;
+	// printf("ids=%d max=%d", idx, getMax(Earliest, N));
+	int max = getMax(Earliest, N);
+	Latest[idx] = max;
+	while(front < rear) {
+		w = Queue[front++];  //处理节点入队， 邻节点入队
+		// printf("w=%d\n");
+		for(int i=1; i<=N; i++) {
+			if(Graph[i][w] != -1) {//i为前节点 
+				// printf("i=%d\n", i);
+				if(Latest[i] >= Latest[w] - Graph[i][w])   //更新最迟时间
+					Latest[i] = Latest[w] - Graph[i][w];
+				if (--Outdegree[i] == 0)
 					Queue[rear++] = i;
-					if(Latest[i] > Latest[w] - Graph[i][w])   //更新最迟时间
-						Latest[i] = Latest[w] - Graph[i][w];
-				}
 			}
 		}
 	}
 }
 
-/* 获取未记录最迟时间的终点 */
-int GetEndPoint(int N)
+int getMax( int arr[], int N)
 {
-	for(int i=1; i<=N; i++)
-		if(Outdegree[i] == 0 && Latest[i] == INT_MAX)
-			return i;
-	return -1;
+	int max = 0;
+	for(int i = 1; i <= N; i++)
+		if( max < arr[i] ){
+			max = arr[i];
+			idx = i;
+		}
+	return max;
 }
 
 void PrintKeyTask(int N)
 {
 	for (int i=1; i<=N; i++) {
-		for (int j=1; j<=N; j++) {
+		for (int j=N; j>=1; j--) {
 			if( Graph[i][j] != -1 ) {
 				if(Latest[j] - Earliest[i] - Graph[i][j] == 0) {
 					printf("%d->%d\n", i, j);
