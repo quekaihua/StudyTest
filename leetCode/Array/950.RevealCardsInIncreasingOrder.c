0bc6ec79e360e458f2a4d6defd1e7d160ed05875
kh <myshengun163.com>
2019-09-30 18:59:20 +0800

    ..

---
+++ leetCode/Array/950.RevealCardsInIncreasingOrder.c
@@ -0,0 +1,16 @@
+
+void putBottom(int Queue[], int Size) {
+
+}
+
+int DeQueue(int Queue[]);
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* deckRevealedIncreasing(int* deck, int deckSize, int* returnSize){
+	int Queue[deckSize];
+	
+
+}
+
